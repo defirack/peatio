b34c471064dae47abf0971f73572083ada793154
Chumak Nadiia <chumaknadya19@gmail.com>
2021-02-12 14:03:14 +0200

    Feature: Move irix to peatio lib

--- config/initializers/upstreams.rb
+++ config/initializers/upstreams.rb
@@ -1,3 +1,7 @@
 require 'peatio/upstream/opendax'
+require 'peatio/irix/houbi'
+require 'peatio/irix/bitfinex'
 
 Peatio::Upstream.registry[:opendax] = Peatio::Upstream::Opendax
+Peatio::Upstream.registry[:bitfinex] = Irix::Bitfinex
+Peatio::Upstream.registry[:huobi] = Irix::Huobi
